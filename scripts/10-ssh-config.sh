e395f891bb714e35f51809cc31902c5bfd029598
Mike Perry <mike@bitbistro.org>
2021-02-08 22:03:11 -0500

    properly refactor later, but adding in support for other packages
    ssh tweak
    
    Take into account different terminals types

--- scripts/10-ssh-config.sh
+++ scripts/10-ssh-config.sh
@@ -3,16 +3,15 @@
 SSH_CONFIG="${HOME}/.ssh/config"
 exec 11>>"$SSH_CONFIG"
 
-
-if ! /bin/egrep -q '^ *LogLevel' $SSH_CONFIG 2>/dev/null ; then
-    echo 'LogLevel ERROR' >&11
-fi
-
-if ! /bin/egrep -q '^ *ServerAliveInterval' $HOME/.ssh/config 2>/dev/null ; then
+if ! command grep -q '^ *ServerAliveInterval' $HOME/.ssh/config 2>/dev/null ; then
     echo 'ServerAliveInterval 60' >&11
 fi
 
-if ! /bin/egrep -q UPDATESTARTUPTTY $HOME/.ssh/config 2>/dev/null ; then
+if ! command grep -q '^ *VisualHostKey' $HOME/.ssh/config 2>/dev/null ; then
+    echo 'VisualHostKey yes' >&11
+fi
+
+if ! command grep -q UPDATESTARTUPTTY $HOME/.ssh/config 2>/dev/null ; then
     echo '# https://bugzilla.mindrot.org/show_bug.cgi?id=2824#c9' >&11
     echo 'Match host * exec "gpg-connect-agent UPDATESTARTUPTTY /bye"' >&11
 fi
