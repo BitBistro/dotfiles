a8307da2f6852428c42c539e7b6f1f0798abde10
Mike Perry <mike@bitbistro.org>
2021-02-08 22:03:11 -0500

    properly refactor later, but adding in support for other packages
    ssh tweak
    
    Take into account different terminals types

--- bin/add-essentials.sh
+++ bin/add-essentials.sh
@@ -7,7 +7,7 @@ BASEDIR="$(readlink -f `dirname  $0`/..)";
 if [ `id -u` -ne '0' ]; then
     if [ -z $NO_RECURSE ]; then
         export NO_RECURSE=1
-        exec sudo /bin/bash -e "$0"
+        exec sudo LEVEL=$LEVEL /bin/bash -e "$0"
     else
         echo "This must be run as root" >&2 && false
     fi
@@ -18,7 +18,27 @@ trap '/bin/rm -f -- "$TEMPFILE"' EXIT
 apt-config dump | egrep 'APT::Install' | sed -re 's/1/0/g' > $TEMPFILE
 export APT_CONFIG="$TEMPFILE"
 
-/usr/bin/apt-get update
-/usr/bin/apt-get -y install aptitude aptitude-doc-en
-/usr/bin/aptitude -y safe-upgrade
-/usr/bin/aptitude -y install "?and(?architecture(native),?or(~prequired))" bash-completion vim-nox git rsync
+if [ -z "$LEVEL" ]; then
+    LEVEL="base"
+fi
+
+case $LEVEL in
+    "base")
+        apt-get update
+        apt-get -y install aptitude aptitude-doc-en
+        aptitude update
+        aptitude -y safe-upgrade
+        aptitude -y install "?and(?architecture(native),?or(~prequired))" bash-completion vim-nox git rsync pinentry-tty\
+                pinentry-curses_ gpg-agent
+    ;;
+    "standard")
+        aptitude update
+        aptitude -y safe-upgrade
+        aptitude -r install "?and(?architecture(native),?or(~prequired,~pimportant,~pstandard),?not(rsyslog))" bsd-mailx
+    ;;
+    *)
+        echo "Not implemented" >&2
+    exit 1
+esac
+
+exit 0
--- scripts/10-gnupg-config.sh
+++ scripts/10-gnupg-config.sh
@@ -11,6 +11,20 @@ if ! grep -q default-cache-ttl "$HOME/.gnupg/gpg-agent.conf" 2>/dev/null; then
     RELOAD_AGENT=true
 fi
 
+if [ -n "$LC_MESSAGES" ]; then
+    if ! grep -q lc-messages "$HOME/.gnupg/gpg-agent.conf" 2>/dev/null; then
+        echo "lc-messages $LC_MESSAGES" >> "$HOME/.gnupg/gpg-agent.conf"
+        RELOAD_AGENT=true
+    fi
+fi
+
+if [ -n "$LC_CTYPE" ]; then
+    if ! grep -q lc-ctype "$HOME/.gnupg/gpg-agent.conf" 2>/dev/null; then
+        echo "lc-ctype $LC_CTYPE" >> "$HOME/.gnupg/gpg-agent.conf"
+        RELOAD_AGENT=true
+    fi
+fi
+
 if [ ! -z "$RELOAD_AGENT" ]; then
     gpg-connect-agent <<< RELOADAGENT &>/dev/null
 fi
