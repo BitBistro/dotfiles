942330bb729855242fe93ce8f74e336682d26aa1
Mike Perry <mike@bitbistro.org>
2021-06-05 21:36:28 -0400

    pull.rebase

--- scripts/10-git-config.sh
+++ scripts/10-git-config.sh
@@ -7,4 +7,5 @@ exec 9>&1 1>/dev/null
 git config --global --get user.email || git config --global user.email "mike@bitbistro.org"
 git config --global --get user.name || git config --global user.name "Mike Perry"
 git config --global --get core.pager || git config --global core.pager "$(command -v less) -FRiX"
+git config --global --get pull.rebase || git config --global pull.rebase "false"
 exec 1>&9 9>&-
