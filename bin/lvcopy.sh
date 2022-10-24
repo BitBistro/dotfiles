98942b095dfbb061220e273c3684410e18a365e2
Mike Perry <mike@bitbistro.org>
2022-10-23 20:51:01 -0400

    thin volumes

--- bin/lvcopy.sh
+++ bin/lvcopy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+THIN=
+if [ "$1" == "-t" ]; then
+	THIN=1
+	shift
+fi
+
 if [ -z "$2" ]; then
 	THIS="$(df -hP / | awk '!/Filesystem/ {print $1; exit}')"
 	THAT="$1"
@@ -29,8 +35,17 @@ fi
 set -vex
 journalctl --flush
 journalctl --relinquish-var
-lvconvert --type mirror --alloc anywhere -m1 "$THIS"
-lvconvert --splitmirrors 1 --name "$THAT" "$THIS"
+if [ -n "$THIN" ]; then
+	VOL="$(basename $THIS | sed -re 's/thisbe-//g')"
+	lvcreate -s -n $VOL+snap -L 1G $THIS
+	VOL="$(basename $THAT | sed -re 's/thisbe-//g')"
+	lvcreate -n $VOL -V 20G thisbe/pool0
+	dd if=$THIS+snap of=$THAT bs=4M conv=sparse status=progress
+	lvremove $THIS+snap
+else
+	lvconvert --type mirror --alloc anywhere -m1 "$THIS"
+	lvconvert --splitmirrors 1 --name "$THAT" "$THIS"
+fi
 mkdir -p /mnt/next
 mount "$THAT" /mnt/next
 sed -i~ -re 's#'"$THIS"'#'"$THAT"'#' /mnt/next/etc/fstab
