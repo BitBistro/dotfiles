f218b3bd7ca00c8003f8b2af5bc197eb3117864e
Mike Perry <mike@bitbistro.org>
2026-06-13 16:59:02 -0400

    feat(deb): add pass to standard package level

--- bin/add-deb-packages.sh
+++ bin/add-deb-packages.sh
@@ -29,11 +29,11 @@ case "$LEVEL" in
         aptitude -r install '?and(?architecture(native),?or(~prequired,~pimportant,~pstandard),?not(~v),?not(~slibs))' \
                 bsd-mailx exim4-daemon-light bash-completion vim-nox rsync pinentry-tty gpg-agent patch zip unzip jq \
                 plocate pinentry-curses_ '?and(~n^plymouth_,?not(~v))' neovim restic curl openssl bsdutils ncal rfkill \
-                wpasupplicant w3m parted bc dc kmod btrfs-progs tcpdump wget wodim busybox-static pinentry-fltk
+                wpasupplicant w3m parted bc dc kmod btrfs-progs tcpdump wget wodim busybox-static pinentry-fltk pass
         aptitude unmarkauto '?and(?architecture(native),?or(~prequired,~pimportant,~pstandard),?not(~v),?not(~slibs),~i)' \
                 bsd-mailx exim4-daemon-light bash-completion vim-nox rsync pinentry-tty gpg-agent patch zip unzip jq \
                 plocate neovim restic curl openssl bsdutils ncal rfkill wpasupplicant w3m parted bc dc kmod btrfs-progs \
-                tcpdump wget wodim busybox-static pinentry-fltk
+                tcpdump wget wodim busybox-static pinentry-fltk pass
     ;;
     "extra")
         aptitude install \
