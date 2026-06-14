d99cc9e1b3484e2a3a71bd778367a25e98b26377
Mike Perry <mike@bitbistro.org>
2026-06-14 15:34:38 -0400

    refactor(scripts): create password-store directory and secure its permissions

--- scripts/00-init.sh
+++ scripts/00-init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -m 700 -p ~/.ssh ~/.gnupg ~/.cache/nvim/tmp ~/... ~/C/go/src
+mkdir -m 700 -p ~/.ssh ~/.gnupg ~/.cache/nvim/tmp ~/... ~/C/go/src ~/.password-store ~/C/dev
 touch ~/.env-local
--- scripts/99-fixperms.sh
+++ scripts/99-fixperms.sh
@@ -1,6 +1,7 @@
-command find -P ~/.ssh ~/.gnupg -type d -exec /bin/chmod -v 700 {} \; 
+command find -P ~/.ssh ~/.gnupg ~/.password-store -type d -exec /bin/chmod -v 700 {} \; 
 command find -P ~/.config ~/.cache -type d -exec /bin/chmod 700 {} \; 
-command find -P ~/.ssh ~/.gnupg -type f -exec /bin/chmod -v 600 {} \; -print
+command find -P ~/.ssh ~/.gnupg ~/.password-store -type f -exec /bin/chmod -v 600 {} \; -print
 command find -P ~/.local -maxdepth 2 -type d -exec /bin/chmod 711 {} \;
 chmod 711 ~/C
+chmod 711 ~/C/dev
 chmod 711 ~/C/go
