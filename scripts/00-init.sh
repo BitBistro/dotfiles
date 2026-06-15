598edf88a612a8ea70659c8a7db7809ec10fa926
Mike Perry <mike@bitbistro.org>
2026-06-15 13:23:45 -0400

    fix(init): set ~/C, ~/C/dev, and ~/C/go to 711 in init to avoid permission fight with 99-fixperms

--- scripts/00-init.sh
+++ scripts/00-init.sh
@@ -5,5 +5,5 @@ install -d -m 700 ~/.cache/nvim/tmp
 install -d -m 700 ~/...
 install -d -m 700 ~/C/go/src
 install -d -m 700 ~/.password-store
-install -d -m 700 ~/C/dev
+install -d -m 711 ~/C ~/C/dev ~/C/go
 touch ~/.env-local
