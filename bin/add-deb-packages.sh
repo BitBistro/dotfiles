6a1a66fd1a05379ba55aa196edc8bd525c0db7ef
Mike Perry <mike@bitbistro.org>
2026-06-15 13:52:46 -0400

    refactor(pkg): reconcile audit by adding sudo to base, dbus to standard, imagemagick to extra, and excluding D-Bus from audit

--- bin/add-deb-packages.sh
+++ bin/add-deb-packages.sh
@@ -25,24 +25,24 @@ case "$LEVEL" in
         apt -y upgrade
         apt -y install aptitude aptitude-doc-en
         aptitude -y install "?and(?architecture(native),?or(~prequired))" bash-completion vim-nox git rsync pinentry-tty \
-                pinentry-curses_ gpg-agent
+                pinentry-curses_ gpg-agent sudo
     ;;
     "standard")
         aptitude -r install '?and(?architecture(native),?or(~prequired,~pimportant,~pstandard),?not(~v),?not(~slibs))' \
                 bsd-mailx exim4-daemon-light patch zip unzip jq \
                 plocate '?and(~n^plymouth_,?not(~v))' neovim restic curl openssl bsdutils ncal rfkill \
                 wpasupplicant w3m parted bc dc kmod btrfs-progs tcpdump wget wodim busybox-static pinentry-fltk pass \
-                dbus-user-session ripgrep tree yq
+                dbus dbus-user-session ripgrep tree yq
         aptitude unmarkauto '?and(?architecture(native),?or(~prequired,~pimportant,~pstandard),?not(~v),?not(~slibs),~i)' \
                 bsd-mailx exim4-daemon-light patch zip unzip jq \
                 plocate neovim restic curl openssl bsdutils ncal rfkill wpasupplicant w3m parted bc dc kmod btrfs-progs \
-                tcpdump wget wodim busybox-static pinentry-fltk pass dbus-user-session ripgrep tree yq
+                tcpdump wget wodim busybox-static pinentry-fltk pass dbus dbus-user-session ripgrep tree yq
     ;;
     "extra")
         aptitude install \
             apt-file arch-test autoconf automake autotools-dev build-essential debhelper debian-keyring debootstrap \
             devscripts dh-make dkms dosfstools dpkg-dev dput dupload e2fsprogs-l10n eatmydata equivs fakeroot fancontrol \
-            gdisk gnupg hdparm htop i2c-tools irqbalance lintian shared-mime-info xauth linux-headers-amd64 \
+            gdisk gnupg hdparm htop i2c-tools irqbalance imagemagick lintian shared-mime-info xauth linux-headers-amd64 \
             lm-sensors localepurge manpages-dev mutt net-tools nocache nvme-cli patchutils pbuilder pigz \
             powermgmt-base read-edid screen smartmontools strace thin-provisioning-tools xutils-dev xdg-user-dirs gdb \
             linux-doc info iw bison flex libncurses-dev libelf-dev libssl-dev zstd cpio dwarves xsel upower alsa-utils \
@@ -52,14 +52,14 @@ case "$LEVEL" in
     ;;
     "audit")
         EXPLICIT_LIST=(
-            aptitude aptitude-doc-en bash-completion vim-nox git rsync pinentry-tty gpg-agent
+            aptitude aptitude-doc-en bash-completion vim-nox git rsync pinentry-tty gpg-agent sudo
             bsd-mailx exim4-daemon-light patch zip unzip jq plocate neovim restic curl openssl
             bsdutils ncal rfkill wpasupplicant w3m parted bc dc kmod btrfs-progs tcpdump wget
-            wodim busybox-static pinentry-fltk pass dbus-user-session ripgrep tree yq
+            wodim busybox-static pinentry-fltk pass dbus dbus-user-session ripgrep tree yq
             apt-file arch-test autoconf automake autotools-dev build-essential debhelper
             debian-keyring debootstrap devscripts dh-make dkms dosfstools dpkg-dev dput
             dupload e2fsprogs-l10n eatmydata equivs fakeroot fancontrol gdisk gnupg hdparm
-            htop i2c-tools irqbalance lintian shared-mime-info xauth linux-headers-amd64
+            htop i2c-tools irqbalance imagemagick lintian shared-mime-info xauth linux-headers-amd64
             lm-sensors localepurge manpages-dev mutt net-tools nocache nvme-cli patchutils
             pbuilder pigz powermgmt-base read-edid screen smartmontools strace
             thin-provisioning-tools xutils-dev xdg-user-dirs gdb linux-doc info iw bison
@@ -70,7 +70,7 @@ case "$LEVEL" in
         )
         manual_pkgs="$(aptitude search -F '%p' '?and(?installed, ?not(?automatic), ?not(~slibs), ?not(~v))' | sort -u)"
         priority_pkgs="$(aptitude search -F '%p' '?and(?installed, ?or(~prequired,~pimportant,~pstandard))' | sort -u)"
-        pattern_pkgs="$(aptitude search -F '%p' '?and(?installed, ?or(~n^plymouth_, ~n^mesa))' | sort -u)"
+        pattern_pkgs="$(aptitude search -F '%p' '?and(?installed, ?or(~n^plymouth_, ~n^mesa, ~n^dbus))' | sort -u)"
         explicit_pkgs="$(printf '%s\n' "${EXPLICIT_LIST[@]}" | sort -u)"
 
         allowed_pkgs="$(printf '%s\n' "$priority_pkgs" "$pattern_pkgs" "$explicit_pkgs" | sort -u)"
